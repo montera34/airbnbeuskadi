01df006b6bbdac6ad34a6d266a631a26147cd9d8
numeroteca <pablo@basurama.org>
2018-06-20 12:39:19 +0200

    barras y mapas análisis airbnb 2017-2018 donostia

--- analisis/habitaciones-vs-viviendas_airbnb.R
+++ analisis/habitaciones-vs-viviendas_airbnb.R
@@ -1,13 +1,15 @@
 # Habitaciones vs Viviendas completas en Airbnb Donostia
 
-# Load libraries
+# ---- Load libraries -----
 library(tidyverse)
 # read shapes
 library(rgdal)
 library(ggmap) #for theme nothing
 library(reshape)
 
-# Load barrios
+
+# ------ Load files ----------
+# Load shapes
 barrios <- readOGR("data/barrios-donostia.geojson")
 menores <- readOGR("data/original/shapes/unidades-menores-donostia.geojson")
 mar <- readOGR("data/original/shapes/mar-donostia.geojson")
@@ -28,6 +30,8 @@ load("data/output/180423_listings-airbnb-donostia_datahippo_barrio-umenor.Rda")
 # Translate room type
 levels(airbnb$room_type) <- c("Vivienda completa","Habitación privada","Habitación compartida")
 
+# ----- Prepares data -------
+
 # Fixes barrios and unidades menores
 airbnb <- transform(airbnb,barrio=unlist(barrio))
 airbnb <- transform(airbnb,umenores=unlist(umenores))
@@ -37,8 +41,6 @@ table(airbnbmerged$room_type)
 table(airbnb$room_type)
 
 # ----- Comparative room types -----------
-
-
 # Compare two data sets by number of room types
 # compare_room <- merge(data.frame(table(airbnb$room_type)),data.frame(table(airbnbmerged$room_type)),by="Var1")
 # compare_room <- merge(compare_room,data.frame(table(airbnb2017$room_type)),by="Var1")
@@ -70,11 +72,8 @@ png(filename="images/hab-viv-barras-airbnb-donostia-2017-2018.png",width = 600,h
          caption = "Datos: Insideairbnb (marzo 2017) y Datahippo (abril 2017, abril 2018). Gráfico: lab.montera34.com/airbnb") +
     coord_flip()
 dev.off()
-# +
-#   geom_text(aes(label = Value),
-#             position = "dodge", stat="identity", size=3,color="#FFFFFF")
 
-# ------------tablas por tipos de habitación-------
+# ------------barras por tipos de habitación-------
 airbnb1_2017 <- airbnb2017 %>% 
   group_by(room_type) %>% 
   summarise(count=n()) 
@@ -98,7 +97,7 @@ airbnb1 <- airbnb %>%
   group_by(room_type) %>% 
   summarise(count=n())
 
-png(filename="images/hab-viv-barras-airbnb-donostia-2018.png",width = 600,height = 400)
+png(filename="images/hab-viv-barras-airbnb-donostia-2018.png",width = 900,height = 600)
 ggplot(airbnb1,aes(x = room_type, y = count)) +
   geom_bar(stat="identity")+
   theme_minimal(base_family = "Roboto Condensed", base_size = 14) +
@@ -113,44 +112,123 @@ ggplot(airbnb1,aes(x = room_type, y = count)) +
   coord_flip()
 dev.off()
 
-# ------------tablas por barrios-------
+# ------------barras por barrios-------
+airbnb3 <- airbnbmerged  %>% 
+  group_by(barrio,room_type) %>% 
+  summarise(count=n()) %>% 
+  mutate(suma=sum(count)) %>%
+  arrange(-count)
+# TODO: hay varios anuncios clasificados como barrio "NA"
+png(filename="images/hab-viv-barras-airbnb-barrios-donostia-2017.png",width = 900,height = 700)
+ggplot(airbnb3,aes(x = reorder(barrio,suma), y = count,fill=room_type)) +
+  geom_bar(stat="identity")+
+  ylim(c(0,800))+
+  theme_minimal(base_family = "Roboto Condensed", base_size = 14) +
+  theme(
+    panel.grid.minor.y = element_blank(), panel.grid.major.y = element_blank(),
+    legend.position="top"
+  ) +
+  labs(title = "Habitaciones y viviendas de Airbnb por barrios en Donostia.",
+       subtitle = "Abril 2017",
+       y = "número de anuncios",
+       x = NULL,
+       caption = "Datos: Insideairbnb y datahippo.org. Gráfico: lab.montera34.com/airbnb") +
+  coord_flip()
+dev.off()
+
 airbnb2 <- airbnb %>% 
-  group_by(barrio,umenores,room_type) %>% 
+  group_by(barrio,room_type) %>% 
   summarise(count=n()) %>% 
   mutate(suma=sum(count)) %>%
   arrange(-count)
 
-ggplot(airbnb2,aes(x = barrio, y = count,fill=room_type)) +
+png(filename="images/hab-viv-barras-airbnb-barrios-donostia-2018.png",width = 900,height = 700)
+ggplot(airbnb2,aes(x = reorder(barrio,suma), y = count,fill=room_type)) +
   geom_bar(stat="identity")+
+  ylim(c(0,800))+
   theme_minimal(base_family = "Roboto Condensed", base_size = 14) +
   theme(
-    panel.grid.minor.y = element_blank(), panel.grid.major.y = element_blank()
+    panel.grid.minor.y = element_blank(), panel.grid.major.y = element_blank(),
+    legend.position="top"
   ) +
   labs(title = "Habitaciones y viviendas de Airbnb por barrios en Donostia.",
-       subtitle = "Abril  2018",
-       x = "número de anuncios",
-       y = NULL,
+       subtitle = "Abril 2018",
+       y = "número de anuncios",
+       x = NULL,
        caption = "Datos: datahippo.org. Gráfico: lab.montera34.com/airbnb") +
   coord_flip()
+dev.off()
+
+# ------------barras por unidades menores-------
+airbnb3 <- airbnbmerged  %>% 
+  group_by(umenores,room_type) %>% 
+  summarise(count=n()) %>% 
+  mutate(suma=sum(count)) %>%
+  arrange(-count)
+# TODO: hay varios anuncios clasificados como barrio "NA"
+png(filename="images/hab-viv-barras-airbnb-umenores-donostia-2017.png",width = 900,height = 1400)
+ggplot(airbnb3,aes(x = reorder(umenores,suma), y = count,fill=room_type)) +
+  geom_bar(stat="identity")+
+  ylim(c(0,600))+
+  theme_minimal(base_family = "Roboto Condensed", base_size = 14) +
+  theme(
+    panel.grid.minor.y = element_blank(), panel.grid.major.y = element_blank(),
+    legend.position="top"
+  ) +
+  labs(title = "Habitaciones y viviendas de Airbnb por unidad menor en Donostia.",
+       subtitle = "Abril 2017",
+       y = "número de anuncios",
+       x = NULL,
+       caption = "Datos: Insideairbnb y datahippo.org. Gráfico: lab.montera34.com/airbnb") +
+  coord_flip()
+dev.off()
+
+airbnb2 <- airbnb %>% 
+  group_by(umenores,room_type) %>% 
+  summarise(count=n()) %>% 
+  mutate(suma=sum(count)) %>%
+  arrange(-count)
+
+png(filename="images/hab-viv-barras-airbnb-umenores-donostia-2018.png",width = 900,height = 1400)
+ggplot(airbnb2,aes(x = reorder(umenores,suma), y = count,fill=room_type)) +
+  geom_bar(stat="identity")+
+  ylim(c(0,600))+
+  theme_minimal(base_family = "Roboto Condensed", base_size = 14) +
+  theme(
+    panel.grid.minor.y = element_blank(), panel.grid.major.y = element_blank(),
+    legend.position="top"
+  ) +
+  labs(title = "Habitaciones y viviendas de Airbnb por unidad menor en Donostia.",
+       subtitle = "Abril 2018",
+       y = "número de anuncios",
+       x = NULL,
+       caption = "Datos: datahippo.org. Gráfico: lab.montera34.com/airbnb") +
+  coord_flip()
+dev.off()
 
 # -----------Mapa---------------
+png(filename="images/hab-viv-mapa-airbnb-menores-donostia-2018.png",width = 800,height = 600)
 ggplot() + 
   scale_fill_manual(values=cbPalette) +
   geom_point(data=airbnb,aes(x=longitude, y=latitude,colour=room_type),
-             alpha=0.5,size = 1)+
+             alpha=0.5,size = 1.5)+
   geom_path(data=menores,aes(x=long, y=lat,group=group), colour="black",size = 0.1)+
   theme_minimal(base_family = "Roboto Condensed", base_size = 14) +
   # theme_nothing(legend = TRUE) +
   # theme_bw() +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        legend.position="top",
+        axis.text = element_blank()) +
   # coord_quickmap(xlim=c(-2.08, -1.92), ylim=c(43.2775,43.335)) + #todo donostia
   coord_quickmap(xlim=c(-2.02, -1.96), ylim=c(43.30,43.331)) + #zoom 
   # coord_fixed() +
   # xlim(-1.968,-1.94) +
   # ylim(43.305,43.325) +
   guides(colour = guide_legend(override.aes = list(alpha = 0.9))) +
-  labs(title = "Habitaciones y viviendas de uso turístico según estado de tramitación",
-       subtitle = "Un total de 1869 anuncios, 1.390 apartamentos comleto, 471 habitaciones y 8 hab. compartidas.",
+  labs(title = "Anuncios de Airbnb según estado de tipo (abril 2018)",
+       subtitle = "Un total de 1869 anuncios: 1.390 apartamentos, 471 habitaciones y 8 habitaciones compartidas.",
        x = NULL,
        y = NULL,
        caption = "Datos: datahippo.org. Gráfico: lab.montera34.com/airbnb")
+dev.off()
