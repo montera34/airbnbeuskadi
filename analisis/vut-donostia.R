d5a2dee10b39d3eb45540c9a85def501c1565cf9
numeroteca <pablo@basurama.org>
2018-07-02 17:36:00 +0200

    vut map analysis in Donostia

--- analisis/vut-donostia.R
+++ analisis/vut-donostia.R
@@ -2,19 +2,26 @@
 # Este archivo genera mapas basados en los datos oficiales de viviendas y habitaciones turísticas
 # según la web del Ayuntamiento de Donostia (descargado en data/output/vut-donostia/censo-viviendas-turisticas-donostia-180301_WGS84.csv).
 
-# Load libraries
+# ------ Load libraries------
 library(tidyverse)
 # read shapes
 library(rgdal)
 library(ggmap) #for theme nothing
 library(gsubfn) #for selecting estado
 
-# Load barrios
+# ------ Load shapes ------
 barrios <- readOGR("data/barrios-donostia.geojson")
 menores <- readOGR("data/original/shapes/unidades-menores-donostia.geojson")
+mar <- readOGR("data/original/shapes/mar-donostia.geojson")
+rio <- readOGR("data/original/shapes/rio-donostia.geojson")
 
 # Load VUT
 vut <- read.delim("data/output/vut-donostia/censo-viviendas-turisticas-donostia-180301_barrio-umenor.csv",sep = ",", encoding = "utf-8")
+
+# Load zonas VUT
+zona_saturada <- readOGR("data/original/vut-donostia/VUT_saturada-donostia-WGS84.geojson")
+zona_b <- readOGR("data/original/vut-donostia/VUT_zonaB-donostia-WGS84.geojson")
+zona_c <- readOGR("data/original/vut-donostia/VUT_zonaC-donostia-WGS84.geojson")
 
 # Corrige nombre de unidad menor
 levels(vut$umenores)[levels(vut$umenores)=="Sag�es"] <- "Sagües"
@@ -26,6 +33,10 @@ vut$estadox <- strapplyc( as.character(vut$estado), ".*_(.*)", simplify = TRUE)
 # add noise to location of points
 vut$latr <- jitter(vut$latitude, factor=1, amount = 0.001)
 vut$lonr <- jitter(vut$longitude, factor=1, amount = 0.001)
+
+table(vut$estado)
+table(vut$estadox)
+table(vut$tipo)
 
 # ----- barras por barrios-------
 vut2 <- vut %>%
@@ -119,26 +130,42 @@ ggplot(vut3 ,aes(x = tipo, y = count,fill=estadox)) +
 dev.off()
 
 # --------------- Mapas --------------------
+
+cbPalette <- c("11d262","f9c21a")
+
 ggplot() + 
+  # mar y río
+  geom_polygon(data=mar,aes(x=long, y=lat,group=group),fill="#aaaaff",alpha=0.5) +
+  geom_polygon(data=rio,aes(x=long, y=lat,group=group),fill="#aaaaff",alpha=0.5) +
+  # zonas de VUT
+  # geom_path(data=zona_saturada,aes(x=long, y=lat,group=group),colour="#000000",size=1,linetype=1) +
+  # geom_path(data=zona_b,aes(x=long, y=lat,group=group),colour="#000000",size=1,linetype=2) +
+  # geom_path(data=zona_c,aes(x=long, y=lat,group=group),colour="#000000",size=1,linetype=3) +
+  geom_polygon(data=zona_saturada,aes(x=long, y=lat,group=group),fill="#bbbbbb",size=1,linetype=1) +
+  geom_polygon(data=zona_b,aes(x=long, y=lat,group=group),fill="#dddddd",size=1,linetype=2) +
+  geom_polygon(data=zona_c,aes(x=long, y=lat,group=group),fill="#efefef",size=1,linetype=3) +
+  # Puntos VUT
   scale_fill_manual(values=cbPalette) +
   geom_point(data=vut,aes(x=lonr, y=latr,colour=estadox,shape=tipo),
-             alpha=0.5,size = 3)+
+             alpha=0.9,size = 2)+
   geom_path(data=menores,aes(x=long, y=lat,group=group), colour="black",size = 0.1)+
   theme_minimal(base_family = "Roboto Condensed", base_size = 14) +
   # theme_nothing(legend = TRUE) +
   # theme_bw() +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(),
+    axis.text = element_blank()) +
   # coord_quickmap(xlim=c(-2.08, -1.92), ylim=c(43.2775,43.335)) + #todo donostia
   coord_quickmap(xlim=c(-2.02, -1.96), ylim=c(43.30,43.331)) + #zoom 
-  # coord_fixed() +
-  # xlim(-1.968,-1.94) +
-  # ylim(43.305,43.325) +
   guides(colour = guide_legend(override.aes = list(alpha = 0.9))) +
   labs(title = "Habitaciones y viviendas de uso turístico según estado de tramitación",
      subtitle = "Un total de 1242 elementos. 1117 viviendas (764 en tramitación) y 125 habitaciones (73 en tramitación).",
      x = NULL,
      y = NULL,
      caption = "Datos: Ayuntamiento de Donostia. Gráfico: lab.montera34.com/airbnb")
+
+
 
 ggplot() + 
   scale_fill_manual(values=cbPalette) +
