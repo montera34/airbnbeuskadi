a0120c3a0fa594fde98c65ac9ade178997c4f7ea
numeroteca <pablo@basurama.org>
2018-09-13 14:09:51 +0200

    prepare results for last review analysis

--- analisis/last-review-analisis.R
+++ analisis/last-review-analisis.R
@@ -5,15 +5,19 @@ library(tidyverse)
 # read shapes
 library(gsubfn) # select text in the parenthesis with regex
 
-# Load data
+# Load data -------
 last_review  <- read.delim("data/output/180604_listings-airbnb-donostia_datahippo_with-last-review-180911.csv",sep = ",")
 last_review  <- read.delim("data/output/180604_listings-airbnb-donostia_datahippo_with-last-review-180911-reviewed.csv",sep = ",")
 
-last_review  <- read.delim("data/output/180618_listings-airbnb-bilbao_datahippo.csv_with-last-review-180912.csv",sep = ",")
-last_review  <- read.delim("data/output/180618_listings-airbnb-bilbao_datahippo.csv_with-last-review-180912-reviewed.csv",sep = ",")
+last_review  <- read.delim("data/output/180618_listings_airbnb-bilbao_datahippo_with-last-review-20180912.csv",sep = ",")
+last_review  <- read.delim("data/output/180618_listings_airbnb-bilbao_datahippo_with-last-review-20180912-reviewed.csv",sep = ",")
 
 
-# Prepare data
+# Create 
+results <- data.frame(matrix(ncol = 4 ))
+colnames(results) <- c("listings_total","con_fechas_disponibles","found_url","last_review_since")
+
+# Prepare data --------
 # Create date format for revised
 last_review$revised_date  <- as.Date( paste(
   as.numeric(strapplyc( as.character(last_review$revised), "[0-9]*-[0-9]*-([0-9]*).*", simplify = TRUE)),
@@ -32,12 +36,26 @@ last_review$lastreview_date  <- as.Date( paste(
   as.numeric(strapplyc( as.character(last_review$lastreview_20180912), "([0-9]*).*", simplify = TRUE)),
   sep = "" ), "%d/%m/%Y")
 
+# Create date format for last review
+last_review$lastreview_date  <- as.Date( paste(
+  as.numeric(strapplyc( as.character(last_review$lastreview_20180911), "[0-9]*-[0-9]*-([0-9]*).*", simplify = TRUE)),
+  "/",
+  as.numeric(strapplyc( as.character(last_review$lastreview_20180911), "[0-9]*-([0-9]*).*", simplify = TRUE)),
+  "/",
+  as.numeric(strapplyc( as.character(last_review$lastreview_20180911), "([0-9]*).*", simplify = TRUE)),
+  sep = "" ), "%d/%m/%Y")
+
 # Número total de anuncios
 nrow(last_review)
+results$listings_total <- nrow(last_review)
+
 
 # Número de disponibles para alquilar a partir de fecha
 nrow(last_review[last_review$revised_date > "2018-03-31",])
 nrow(last_review[last_review$revised_date < "2018-03-31",])
+
+results$con_fechas_disponibles <- nrow(last_review[last_review$revised_date > "2018-03-31",])
+
 
 # Cuántos pisos existe su URL
 table(last_review$exists_20180911)
@@ -47,6 +65,10 @@ table(last_review$exists_20180912)
 nrow(last_review[last_review$exists_20180911 == 1,])
 nrow(last_review[last_review$exists_20180912 == 1,])
 
+results$found_url <- nrow(last_review[last_review$exists_20180911 == 1,])
+
+results$found_url <- nrow(last_review[last_review$exists_20180912 == 1,])
+
 
 # cuenta los anuncios con review posterior a una fecha, que exite su URL y que su last_review no es 0 
 # (esto es, que el scraping ha funcionado y captirado esa fecha)
@@ -55,11 +77,26 @@ nrow(last_review[last_review$lastreview_date > "2018-03-31" &
                    last_review$lastreview_20180911 != "0"
                  ,])
 
+results$last_review_since <- nrow(last_review[last_review$lastreview_date > "2018-03-31" & 
+                                                 last_review$exists_20180911 == 1 & 
+                                                 last_review$lastreview_20180911 != "0"
+                                               ,])
+
 nrow(last_review[last_review$lastreview_date > "2018-03-31" & 
                    last_review$exists_20180912 == 1 & 
                    last_review$lastreview_20180912 != "0"
                  ,])
+results$last_review_since <- nrow(last_review[last_review$lastreview_date > "2018-03-31" & 
+                                                last_review$exists_20180912 == 1 & 
+                                                last_review$lastreview_20180912 != "0"
+                                              ,])
 
+# Resultados de calcular pisos activos
+results 
+
+# Resultados en porcentaje
+results_per <- round(results/results[1,1], digits =4) *100
+results_per
 
 
 # De los pisos que existen ¿cuales son habitaciones o pisos completos?
